dffdb7907f8cf370ccd55e488f7b784acbab8993
seeeeba <lillo.sebastian1@gmail.com>
2021-11-15 16:15:17 +0100

    close hamburger menu

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const navBarMenu = document.querySelector('.header-right');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navClose = document.querySelector('.hamburger-menu .close')
 
 //funzione click
 //open hamburger menu
@@ -13,3 +14,11 @@ navBarMenu.addEventListener('click',
     }
 );
 
+//close hamburger menu
+
+navClose.addEventListener("click", 
+    function() {
+        navClose.classList.toggle("active");
+        hamburgerMenu.classList.toggle("active");
+    }
+);
